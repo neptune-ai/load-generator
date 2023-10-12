3d2210cf5169711d90b5fb6d29b60d32dc536afd
dzwiedziu <p@p15z.com>
2023-10-12 12:00:22 -0700

    Better diagnostics and support for parallel syncronization of a run.

--- load_generator.py
+++ load_generator.py
@@ -1,31 +1,32 @@
 import argparse
-import concurrent.futures
 import math
 import multiprocessing as mp
+import neptune
+import logging
 import os
 import random
 import string
+import subprocess
 import time
 
 
-
-
 def _initialize_run(run_name, *args, **kwargs):
-  import neptune
-
   return neptune.init_run(
       capture_stderr=False,
       capture_stdout=False,
       name=run_name,
       source_files=[], # we don't want to send source files
+      git_ref=False, # we don't want to send git info
       enable_remote_signals = False,
       capture_hardware_metrics=False)                                                                                                                                                                                                                                                                                                                      
-                                                                                                                                                                                                                  
+
+
 def log_not_indexed_metrics(run, step, n, seed=0):                                                                                                                                                                       
   r_vals = random.Random(seed)
   for i in range(n):                                                                                                                                                                                          
     run[f"metrics/not_indexed/metric_{i}"].extend([step**1.1 + r_vals.random()])                                                                                                                                                                  
-                                                                                                                                                                                                                  
+
+
 def log_not_indexed_atoms(run, step, n, seed=0):
   r_vals = random.Random(seed)
   for i in range(n):                                                                                                                                                                                          
@@ -78,44 +79,50 @@ def _get_sync_position(runs):
     path = '.neptune/async/run__{}'.format(run._id)
     os.listdir(path)
     path = os.path.join(path, os.listdir(path)[0])
-
-    # trying a few times to avoid race conditions
-    for i in range(0,1000):
-      try:
-        with open(os.path.join(path, 'last_ack_version'), 'r') as f:
-          last_ack = int(f.read().split('\n')[0])
-        with open(os.path.join(path, 'last_put_version'), 'r') as f:      
-          last_put = int(f.read().split('\n')[0])
-        last_acks.append(last_ack)
-        last_puts.append(last_put)
-        break
-      except ValueError:
-        pass
+    partitions = os.listdir(path)
+    if 'partition-0' not in partitions:
+      # we are not usign partitions
+      partitions = ['']
+    for p in partitions:
+      for i in range(0,1000):
+        try:
+          with open(os.path.join(path, p, 'last_ack_version'), 'r') as f:
+            last_ack = int(f.read().split('\n')[0])
+          with open(os.path.join(path, p, 'last_put_version'), 'r') as f:      
+            last_put = int(f.read().split('\n')[0])
+          last_acks.append(last_ack)
+          last_puts.append(last_put)
+          break
+        except ValueError:
+          pass
 
   sum_puts = sum(last_puts)
   sum_acks = sum(last_acks)
   return sum_acks, sum_puts
 
 
-def perform_load_test(n, steps, atoms, series, indexed_split, step_time, run_name='', threads=8, group_name='', color=''):
+def _seconds_to_hms(seconds):
+  return '{}:{:02d}:{:02d}'.format(int(seconds // 3600), int((seconds % 3600) // 60), int(seconds % 60))
 
-  import logging
-  
+def perform_load_test(n, steps, atoms, series, indexed_split, step_time, run_name='', sync_partitions=1, group_name='', color=''):
   logging.getLogger("urllib3.connectionpool").setLevel(logging.ERROR)
 
   log_format = '{}Group={}\x1b[0m - %(levelname)s - %(message)s'.format(color, group_name)
   logging.basicConfig(format=log_format)
   log = logging.getLogger('load_test')
   log.setLevel(logging.INFO)
-  
 
-  runs = []
-  with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-    future_runs = [executor.submit(_initialize_run, run_name) for _ in range(n)]
-    for f in future_runs:
-        runs.append(f.result())                       
+  if sync_partitions > 1:
+    log.warn('Turning experimental mode on with {} partitions.'.format(sync_partitions))
+    os.environ['NEPTUNE_MODE'] = 'experimental'
+    os.environ['NEPTUNE_ASYNC_PARTITIONS_NUMBER'] = str(sync_partitions)
+  else:
+    os.environ.pop('NEPTUNE_MODE', None)
+    os.environ.pop('NEPTUNE_ASYNC_PARTITIONS_NUMBER', None)
 
-  st = time.monotonic()                                                                                                                                                                                              
+  runs = [_initialize_run(run_name) for _ in range(n)]
+
+  start_time = time.monotonic()                                                                                                                                                                                              
 
   # log indexed atoms
   for j in range(n):
@@ -138,33 +145,58 @@ def perform_load_test(n, steps, atoms, series, indexed_split, step_time, run_nam
       puts_per_step = (puts - initial_puts) / (i+1)
       total_puts = int(puts_per_step * steps)
 
-      msg = 'Steps on disk {}/{} ({:.2f}%). '.format(i, steps-1, (i+1)/steps * 100)
+      elapsed_time = after - start_time
+      eta_time = elapsed_time / (i+1) * (steps - i - 1)
+
+      msg = 'Steps on disk {}/{} ({:.2f}%). Elapsed {} - ETA {}. '.format(
+        i, steps-1, (i+1)/steps * 100, _seconds_to_hms(elapsed_time), _seconds_to_hms(eta_time))
       msg = msg + 'Operations synchronized with NPT server {}/{} ({:.2f}%)'.format(acks, total_puts, acks/total_puts * 100)
       log.info(msg)
       
       if after-before > step_time:
-         logging.warn('Writing to a disk is laggging behind. Consider increasing step_time {} -> {:.2f}'.format(step_time, after-before))
+         logging.warn('Writing to a disk is lagging. Consider increasing the step_time {} -> {:.2f}'.format(step_time, after-before))
 
-      time.sleep(max(step_time - (after - before), 0))                                                                                                                                                                       
+      time.sleep(max(step_time - (after - before), 0))
+  
+  # waiting for all data being flashed to a disk
+  time.sleep(10)
+  sync_started_time = time.monotonic()
+  started_acks, puts = _get_sync_position(runs)
+  
+  while True:
+    acks, _ = _get_sync_position(runs)  
+    if acks == puts:
+      break
+    else:
+      if acks > started_acks:
+        current_time = time.monotonic()
+        elapsed = current_time - sync_started_time
+        estimated = elapsed / (acks - started_acks) * (puts - started_acks)
+        seconds_eta = estimated - elapsed
+        # seconds eta in format hh:mm:ss
+        eta_msg = 'ETA {}'.format(_seconds_to_hms(seconds_eta))
+      else:
+        eta_msg = ''
+      log.info('Synchronizing {}/{} ({:.2f}%). {}'.format(acks, puts, acks/puts * 100, eta_msg)) 
+      time.sleep(10)
 
+  stop_started_time = time.monotonic()
 
+  sync_time = stop_started_time - sync_started_time
+  log.info('Synchronization finished in {}'.format(_seconds_to_hms(sync_time)))
 
-  wt = time.monotonic()                                                                                                                                                                                              
+  for r in runs:
+     r.stop()
 
-  with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-      futures = [executor.submit(runs[i].sync) for i in range(n)]
-      for f in futures:
-          f.result()
-                                                                                                                                                                                                                        
-  syncT = time.monotonic()                                                                                                                                                                                           
+  stop_end_time = time.monotonic()
+  stopping_time = stop_end_time - stop_started_time
+  
+  log.info('Summary: total time: {}, training time: {}, syncing time: {}, stopping time {}.'.format(\
+    _seconds_to_hms(stop_end_time - start_time),\
+    _seconds_to_hms(sync_started_time - start_time),\
+    _seconds_to_hms(sync_time),\
+    _seconds_to_hms(stopping_time)))
 
-  with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-      futures = [executor.submit(runs[i].stop) for i in range(n)]
-      for f in futures:
-          f.result()                                                                                                                                                                                                                   
-                                                                                                                                                                          
-  stopT = time.monotonic()                                                                                                                                                                                           
-  log.info('Summary: training {}\tsyncing:{}\tstopping:{}'.format(wt-st, syncT-wt, stopT-syncT))
 
 if __name__ == "__main__":
   argparse = argparse.ArgumentParser()
@@ -175,7 +207,7 @@ if __name__ == "__main__":
   argparse.add_argument("--step-time", type=int, default=1)
   argparse.add_argument("--indexed-split", type=float, default=0.1, help='split of indexed metrics and atoms vs not indexed')
   argparse.add_argument("--run-name", type=str, default='')
-  argparse.add_argument("--threads", type=int, help='per process to initialize, sync and close runs', default=8)
+  argparse.add_argument("--sync-partitions", type=int, help='(experimental) number of threads per run used to sync with NPT servers', default=1)
   argparse.add_argument("--processes", type=int, help='number of processes. In total we will log runs * processes', default=1)
   args = argparse.parse_args()
 
@@ -184,14 +216,29 @@ if __name__ == "__main__":
   atoms = args.atoms
   series = args.series
   step_time = args.step_time
-  threads = args.threads
+  sync_partitions = args.sync_partitions
   run_name = args.run_name
   num_processes = args.processes
   indexed_split = args.indexed_split
 
+  assert(series + atoms <= 99900)
+  assert(int(subprocess.check_output("ulimit -n", shell=True)) > 2000)
+  assert(num_processes * n <= 1200)
+  assert(num_processes * n * sync_partitions <= 2400)
   assert(num_processes > 0 and num_processes <= 160)
-  assert(indexed_split * (atoms + series) <= 9800) # 9800 is a limit of neptune, but
+  assert(indexed_split * (atoms + series) <= 10000) # 10000 is a limit of indexed fields at NPT server
+
+  if series + atoms > 10000 and sync_partitions <= 1:
+     pass
+
   
+  if sync_partitions > 1 and neptune.__version__ != '1.8.3rc1.post10+69d7735':
+    
+    logging.error('You need to have experimental version of NPT client installed. Run\n'\
+              '  pip install git+https://github.com/neptune-ai/neptune-client.git@partitioned')
+    exit(1)
+
+
   colors = [] 
   for fg in range(30,38):
       s1 = ''
@@ -203,7 +250,7 @@ if __name__ == "__main__":
 
   processes = [
      mp.Process(target=perform_load_test, args=(
-        n, steps, atoms, series, indexed_split, step_time, '{}-group-{}'.format(run_name, i), threads, i, colors[i % len(colors)])
+        n, steps, atoms, series, indexed_split, step_time, '{}-group-{}'.format(run_name, i), sync_partitions, i, colors[i % len(colors)])
         ) for i in range(num_processes)]
   
   for i in range(num_processes):
